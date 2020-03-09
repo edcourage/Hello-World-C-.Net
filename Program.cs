73e8a9db7f1f93241cd78bc8171cd33ec88a9f91
Ed Courage <edcourage@live.com>
2020-03-09 12:48:59 +0000

    added date and time

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace helloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var dateAndTime = DateTime.Now;
+            Console.WriteLine($"The time is {dateAndTime.ToString("t")}\nThe date is {dateAndTime.ToString("d")}");
         }
     }
 }
